bbd660c73f318faad29651ebc21552c81ad056c3
jincl <jincl@sh-db.com.cn>
2019-11-05 19:17:06 +0800

    这是asp.net core的测试项目

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        //public string index(){
+        //    return "this is my default action...";
+        //}
+
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1) {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
